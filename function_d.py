edcb95a3e2cb73958a238d2255f278865ff8bbfb
Mikaela Baluyot <mikaebal@gmail.com>
2025-03-24 14:32:56 -0700

    added hi to function d

--- function_d.py
+++ function_d.py
@@ -2,7 +2,9 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+
+    def hi():
+        print("hiii")
 
 
 if __name__ == "__main__":
